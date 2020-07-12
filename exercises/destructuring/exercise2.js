4378661b23a86d369106c21180f1711408105823
Matthew Weaber <matthewweaber89@gmail.com>
2020-07-12 17:12:19 -0400

    destruct

---
+++ exercises/destructuring/exercise2.js
@@ -0,0 +1,9 @@
+const { name, weapon, location } = {
+  name: "Rusty",
+  weapon: "candlestick",
+  location: "kitchen",
+};
+
+console.log(name);
+console.log(weapon);
+console.log(location);
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Functional Programming in JS
+
+
+// ES6 Destructuring
+const { first, second } = {first:1, second:2}
+
+console.log(first, second);
+
+// Declaring variable and then assigning those variables the values we want. 
